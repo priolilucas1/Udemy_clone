5b0ec5f434c899174aa734dddeeae230863ae7e9
priolilucas1 <priolilucas1@gmail.com>
2021-07-05 12:21:36 -0300

    Mobile App Bar Done

---
+++ lib/pages/home/widgets/app_bar/mobile_app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MobileAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Container(
+        height: 35,
+        child: Image.asset('images/flutter_icon.png'),
+      ),
+      centerTitle: true,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.search),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.shopping_cart),
+        )
+      ],
+    );
+  }
+}

9ed188e8a366a7695cc96c13e881a66c5cdc87a9
priolilucas1 <priolilucas1@gmail.com>
2021-07-06 13:53:25 -0300

    Home Page Done

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:udemy_clone/breakpoints.dart';
 import 'package:udemy_clone/pages/home/widgets/app_bar/mobile_app_bar.dart';
 import 'package:udemy_clone/pages/home/widgets/app_bar/web_app_bar.dart';
+import 'package:udemy_clone/pages/home/widgets/sections/advantages_section.dart';
+import 'package:udemy_clone/pages/home/widgets/sections/courses_section.dart';
 import 'package:udemy_clone/pages/home/widgets/sections/top_section.dart';
 
 class HomePage extends StatelessWidget {
@@ -27,6 +29,8 @@ class HomePage extends StatelessWidget {
               child: ListView(
                 children: [
                   TopSection(),
+                  AdvantagesSection(),
+                  CoursesSection(),
                 ],
               ),
             ),
